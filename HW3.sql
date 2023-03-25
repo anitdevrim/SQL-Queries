5c4389e3f0d460c571e84ba0ab9a2229570ef7e9
anitdevrim <58302468+anitdevrim@users.noreply.github.com>
2023-03-25 15:34:06 +0300

    Update HW3.sql

--- HW3.sql
+++ HW3.sql
@@ -1,15 +1,15 @@
 --1
---SELECT country FROM country
---WHERE country LIKE 'A%a'
+SELECT country FROM country
+WHERE country LIKE 'A%a'
 
 --2
---SELECT country FROM country
---WHERE country LIKE '______%n'
+SELECT country FROM country
+WHERE country LIKE '______%n'
 
 --3
---SELECT title FROM film
---WHERE title ILIKE '%t%t%t%t%t'
+SELECT title FROM film
+WHERE title ILIKE '%t%t%t%t%t'
 
 --4
---SELECT * FROM film
---WHERE (title LIKE 'C%') AND (length > 90) AND (rental_rate = 2.99)
+SELECT * FROM film
+WHERE (title LIKE 'C%') AND (length > 90) AND (rental_rate = 2.99)
